dc0ca60a4f58c4f8bb5b01f8a9fe4e98c25df500
VatsalFuria <139171811+VatsalFuria@users.noreply.github.com>
2026-08-19 17:09:40 +0530

    rectifying package name error

--- keeps_application/android/app/build.gradle.kts
+++ keeps_application/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.keeps_application"
+    namespace = "dev.vatsaljf.keeps"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
